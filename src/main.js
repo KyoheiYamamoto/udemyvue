d39d7aaa7a5d5aed4e1ec2d5231e25d96f1b1318
KyoheiYamamoto <arienkyohei@gmail.com>
2021-03-01 10:37:11 +0900

    単一コンポーネント

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import LIkeNumber from './LikeNumber.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.component('LIkeNumber', LIkeNumber);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
